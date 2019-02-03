e091ead97621382a9a9c74a66acdc2a2f174ba8f
SaburoShimizu <44968818+SaburoShimizu@users.noreply.github.com>
2019-02-03 18:31:49 +1000

    Delete TestSuka.lua

--- TestSuka.lua
+++
@@ -1,8 +0,0 @@
-function main()
-    if not isSampfuncsLoaded() or not isSampLoaded() then return end
-    while not isSampAvailable() do wait(100) end
-    sampAddChatMessage('Работает сука блять', 1)
-    while true do
-        wait(0)
-    end
-end
